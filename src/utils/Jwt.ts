e3b6feb1851aaebda1b6de6960012e8dcfa4d1e7
Neetesh Parihar <neeteshparihar2212@gmail.com>
2026-05-24 15:43:55 +0530

    use the environment variable to set sameSite and secure flags for production and localhost

--- src/utils/Jwt.ts
+++ src/utils/Jwt.ts
@@ -8,7 +8,7 @@ interface IPayload {
 }
 
 const SECRET = process.env.SECRET_KEY as string;
-const REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET as string; 
+const REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET as string;
 const time = (process.env.JWT_EXPIRY || "15m") as any;
 
 export const generateJwt = (payload: IPayload): string => {
@@ -25,21 +25,25 @@ export const decode = (token: string): IPayload | null => {
   try {
     // verify() checks the Signature AND the 'exp' claim automatically
     const payload = jwt.verify(token, SECRET);
-    return payload as  IPayload ;
+    return payload as IPayload;
   } catch (error) {
     return null;
   }
 };
 
 export const clearJwt = (res: Response) => {
-  res.clearCookie("jwtToken");
+  res.clearCookie("jwtToken", {
+    httpOnly: true,
+    secure: process.env.app_environment === "production",
+    sameSite: process.env.app_environment === "production" ? "none" : "lax",
+  });
 };
 
 export const setCookie = (token: string, res: Response) => {
   res.cookie("jwtToken", token, {
     httpOnly: true, // Prevents JavaScript access (XSS protection)
-    secure: true, // MUST be true for SameSite="none"
-    sameSite: "none", // Allows cross-site cookie usage
+    secure: process.env.app_environment === "production", // MUST be true for SameSite="none"
+    sameSite: process.env.app_environment === "production" ? "none" : "lax", // Allows cross-site cookie usage
     maxAge: 15 * 60 * 1000, // Match the JWT expiration ( 15 minutes in milliseconds)
   });
 };
@@ -54,7 +58,7 @@ export const generateRefToken = (Payload: { userId: string }): string => {
 export const decodeRefToken = (token: string): IPayload | null => {
   try {
     const payload = jwt.verify(token, REFRESH_SECRET);
-    return payload as  IPayload ;
+    return payload as IPayload;
   } catch (error) {
     return null;
   }
@@ -63,8 +67,8 @@ export const decodeRefToken = (token: string): IPayload | null => {
 export const setRefToken = (refToken: string, res: Response) => {
   res.cookie("refToken", refToken, {
     httpOnly: true,
-    secure: true, // MUST be true for SameSite="none"
-    sameSite: "none", // Allows cross-site cookie usage
+    secure: process.env.app_environment === "production",
+    sameSite: process.env.app_environment === "production" ? "none" : "lax",
     maxAge: 24 * 60 * 60 * 1000,
     path: "/api/user/auth",
   });
@@ -73,17 +77,17 @@ export const setRefToken = (refToken: string, res: Response) => {
 export const clearRefToken = (res: Response) => {
   res.clearCookie("refToken", {
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    secure: process.env.app_environment === "production",
+    sameSite: process.env.app_environment === "production" ? "none" : "lax",
     maxAge: 24 * 60 * 60 * 1000,
     path: "/api/user/auth",
   });
 };
 
 export const generateResetToken = (email: string): string => {
-  const key = process.env.RESET_TOKEN_KEY as string; 
-  if( !key ) throw new Error("Reset token key is not defined");
-  const token = jwt.sign({ email }, key , {
+  const key = process.env.RESET_TOKEN_KEY as string;
+  if (!key) throw new Error("Reset token key is not defined");
+  const token = jwt.sign({ email }, key, {
     expiresIn: "10m",
   });
   return token;
